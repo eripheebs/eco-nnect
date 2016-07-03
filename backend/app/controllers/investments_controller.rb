5f8b82104897301d9993274bbba373f3dad70c3a
eripheebs <erikapheby@gmail.com>
2016-07-03 16:12:40 +0100

    creating investment passing test

--- backend/app/controllers/investments_controller.rb
+++ backend/app/controllers/investments_controller.rb
@@ -1,2 +1,13 @@
 class InvestmentsController < ApplicationController
+  def create
+    investment = Investment.new(investment_params)
+    render json:investment if investment.save
+  end
+
+  private
+
+  def investment_params
+    params.require(:investment).permit(:industry, :description, :ngo)
+  end
+
 end
