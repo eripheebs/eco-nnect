483dbfe9e2b144bd97ef639a386dd58198df6fd1
eripheebs <erikapheby@gmail.com>
2016-07-09 16:52:21 +0100

    react components working in rails

--- backend/app/assets/javascripts/components.js
+++ backend/app/assets/javascripts/components.js
@@ -1,7 +1,9 @@
-//= require react
+// app/assets/javascripts/components.js
+//= require_self
 //= require react_ujs
 
 React = require('react');
+ReactDOM = require('react-dom');
 
 // put components here
-DemoComponent = require('./components/DemoComponent');
+DemoComponent = require('./components/DemoComponent.jsx');
