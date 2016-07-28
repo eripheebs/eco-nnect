c35aaa414ec231f475268adb13494e859682ce30
eripheebs <erikapheby@gmail.com>
2016-07-28 13:07:14 +0100

    webpack start script added and incorrect file path changed

--- react_frontend/app/index.js
+++ react_frontend/app/index.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var HelloComponent = require('components/HelloComponent');
+var HelloComponent = require('./components/HelloComponent');
 
 ReactDOM.render(
   <HelloComponent />,
